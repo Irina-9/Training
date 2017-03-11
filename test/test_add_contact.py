79ca87ba827dad4bb8edd97f301ebbbf4bf02d5a
Irina <emeraude20973@mail.ru>
2017-03-11 23:32:15 +0300

    Выделены 2 класса для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,13 +13,13 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="werert", middlename="eretr", lastname="rtetre",
-                                nickname="erertt", title="tttttt", company="rrrrr",
-                                address="etrtytyret", phone_home="35456", email="klnlknnlgf"))
+    app.contact.create(Contact(firstname="werert", middlename="eretr", lastname="rtetre",
+                       nickname="erertt", title="tttttt", company="rrrrr",
+                       address="etrtytyret", phone_home="35456", email="klnlknnlgf"))
     app.session.logout()
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="",
-                                company="", address="", phone_home="", email=""))
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="",
+                       company="", address="", phone_home="", email=""))
     app.session.logout()
